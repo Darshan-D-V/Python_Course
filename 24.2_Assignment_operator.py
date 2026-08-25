4442cfbec20b90ea843f487e930ef26bcac76e94
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:36:00 +0530

    added assignment operator

---
+++ 24.2_Assignment_operator.py
@@ -0,0 +1,36 @@
+#Assignment Operators:-
+#Add and Assign
+a=10
+b=2
+a+=b
+print(a)
+#Subtract and Assign
+c=40
+d=3
+c-=d
+print(c)
+#Multiply and Assign
+e=3
+f=4
+e*=f
+print(e)
+#Divide and Assign
+g=12
+h=5
+g/=h
+print(g)
+#Floor Division and Assign
+i=12
+j=5
+i=i//j
+print(i)
+#Modulus and Assign
+k=40
+l=15
+k%=l
+print(k)
+#Power and Assign
+m=2
+n=3
+m**=n
+print(m)
