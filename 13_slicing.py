761d0c4798cafd747749b7e707216c45a2ddefc5
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:25:12 +0530

    added slicing

---
+++ 13_slicing.py
@@ -0,0 +1,10 @@
+l=[10,20,30,40,50,60,70,80,90]
+print(l[2:5])
+print(l[1:6:2])
+print(l[2:len(l):3])
+print(l[7:4:-1])
+print(l[-1:-8:-3])
+print(l[-1:-len(l)-1:-1])#Default for negative[-1:-len(l)-1:-1]
+
+roll_no="4GM22CS031"
+print(roll_no[5:7])
