95d0408551eb7a016a747a11893b1c8a1b4517c2
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:59:05 +0530

    added while loop

---
+++ 28_while_loop.py
@@ -0,0 +1,26 @@
+'''
+count=0#initialization
+while count<5:#condition
+    print("Hello")
+    count+=1#updation
+
+i=87
+while i<=95:
+    print(i)
+    i+=2
+
+i=8
+while i>1:
+    print(i)
+    i-=1
+
+i=80
+while i>61:
+    print(i)
+    i-=3
+'''
+l=[90,80,70,60,50,40,30,20,10]#90,70,50,30
+i=0
+while i<7:
+    print(l[i])
+    i=i+2
