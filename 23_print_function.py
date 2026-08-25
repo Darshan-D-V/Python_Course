25cfc07147faa57ba186806928cdc9ab132e4fb6
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:34:12 +0530

    added print function

---
+++ 23_print_function.py
@@ -0,0 +1,15 @@
+#print() function
+#1]sep=' ',2]end='\n'
+#1]sep:-syntax:-print(v1,v2,v3,sep=' ')
+#default value is space,we can give whatever we want.
+print(10,40,70,30,50,20,sep="@")
+print(10,40,70,30,50,20,sep="\n")
+
+print("============================================")
+
+#2]end='\n'
+#syntax:-print(v1,v2,v3,end='\n')
+print(10,20,30,sep='\n',end=' ')
+print(40,sep='',end='@')
+print(50,60,70,sep='\n',end='\n')
+print("stop")
