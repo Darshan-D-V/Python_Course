e7706c37906a1a2ede3b09e10e9cc1183a3c641c
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:37:18 +0530

    added logical operator

---
+++ 24.4_Logical_operators.py
@@ -0,0 +1,48 @@
+#Logical operators:-
+#or
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print("==============================")
+
+#and
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print("==============================")
+
+#not
+print(not False)
+print(not True)
+
+print("=============================")
+
+#A and B
+#Generalization
+print(8 and 16 and 0)
+print(16 and 8)
+print(1 and 0)
+print(0.1 and 0.5)
+print([] and [0,0])
+print(True and "True")
+print(None and "None")
+
+print("=============================")
+
+#A or B
+#Generalization
+print(8 or 16)
+print(16 or 8)
+print(1 or 0)
+print(0.1 or 0.5)
+print([] or [0,0])
+print("False" or "True")
+print({} or {1,2,3})
+print([] or {} or {1,2})
+
+
+
