65876576aae3f9f1cd0a0ecb3326e8a8483a65d5
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:35:25 +0530

    added arithmetic operator

---
+++ 24.1_Arithmetic_operator.py
@@ -0,0 +1,13 @@
+#Arithmetic Operator
+a=101
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)#quotient with float value
+print(a//b)#quotient with int value
+print(a%b)#remainder
+print(2**3)
+print(100**(1/2))#sqrt of 100
+print(100**(1/3))#cube root of 100
+print(4.641588833612778**3)
