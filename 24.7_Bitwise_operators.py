424daf87a4898ac4b8479b8fcfeb475bdd9ab99b
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:38:54 +0530

    added bitwise operator

---
+++ 24.7_Bitwise_operators.py
@@ -0,0 +1,35 @@
+#1]Bitwise AND (&) :-
+#it returns 1 if both bits are 1.
+a=12
+b=5
+print(a&b)
+
+
+#2]Bitwise OR (|) :-
+#it returns 1 if atleast one of bits is 1.
+a=12
+b=5
+print(a|b)
+
+
+#3]Bitwise XOR(^) :-
+#it returns 1,if both bits are different .
+a=12
+b=5
+print(a^b)
+
+#4]Bitwise NOT(~) :-
+#it is a unary operator(it works on only one operand),it inverts the bits .
+a=12
+print(~a)
+
+#5]Bitwise Leftshift(<<) :-
+#it is a unary operator(it works on only one operand),Bitwise Leftshift(<<) shifts the bits to left .
+a=12
+print(a<<2)
+
+#6]Bitwise Righttshift(<<) :-
+#it is a unary operator(it works on only one operand),Bitwise Righttshift(>>) shifts the bits to right.
+a=12
+print(a>>2)#var divided by 2 power no of right positions
+
