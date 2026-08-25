faa670dfc9ac1bdae5e5eb36dd201855d3b053af
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:44:43 +0530

    added nested if

---
+++ 25.5_nested_if.py
@@ -0,0 +1,13 @@
+print("start")
+name=input("enter user name")
+
+if name=="Darshan D V":
+    print("username correct")
+    password=input("enter the password")
+    if password=="darshan1234":
+        print("login successfull ")
+    else:
+        print("invalid password")
+else:
+    print("invalid username")
+print("end")
