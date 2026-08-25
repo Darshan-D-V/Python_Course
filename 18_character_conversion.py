5972e3d8bf2ccd2503951a2859607258860aedaa
Darshan D V <darshandarshu546975@gmail.com>
2026-08-25 20:29:48 +0530

    added character conversion

---
+++ 18_character_conversion.py
@@ -0,0 +1,30 @@
+#1]ord("chartacter"):-returns the unicode code point for a one characater.
+'''
+A->65
+Z->90
+a->97
+z->122
+0->48
+9->57
+' ' ->32
+
+'''
+print(ord('A'))
+print(ord('Z'))
+print(ord('a'))
+print(ord('z'))
+print(ord('0'))
+print(ord('9'))
+print(ord(' '))
+
+#2]chr(unicode integer):-
+print(chr(65))
+print(chr(90))
+print(chr(48))
+print(chr(32))
+print(chr(128515))
+print(chr(3271))
+
+
+
+
