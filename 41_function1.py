ce15e7248a2a03f4449df1b43cc6b6a57cde6059
Darshan D V <darshandarshu546975@gmail.com>
2026-08-27 22:23:17 +0530

    added function-1 file

---
+++ 41_function1.py
@@ -0,0 +1,18 @@
+
+#1]function without parameter and without return
+def display():      #function declaration
+    print("thursday")#function body
+
+print(display)#address of display function
+display()    #function call    
+display()
+
+
+def view():
+    print("Darshan")
+view()
+view()
+view()
+
+
+
