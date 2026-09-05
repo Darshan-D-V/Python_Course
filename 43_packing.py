650b8d5331d4be198f534dcbed9b99aa3c0b8e9e
Darshan D V <darshandarshu546975@gmail.com>
2026-09-05 15:05:34 +0530

    added packing file

---
+++ 43_packing.py
@@ -0,0 +1,9 @@
+#implicit packing
+a=10,20,30,40
+print(a)
+print(type(a))#multiple values assigned to a single variable by default into tuple.
+
+
+a1="india","korea","japan"
+print(type(a1))
+print(a1[1])
